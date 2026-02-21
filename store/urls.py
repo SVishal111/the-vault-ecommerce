49f4746dbd4723192da4d18c978fbb03ffc188c9
SVishal111 <vishals18266@gmail.com>
2026-02-21 15:39:32 +0530

    Added Manage Products Functionality

--- store/urls.py
+++ store/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('admin-dashboard/', views.admin_dashboard, name='admin_dashboard'),
     path('register/', views.register, name='register'),
     path('redirect-after-login/', views.redirect_after_login, name='redirect_after_login'),
+path('dashboard/products/', views.admin_product_list, name='admin_product_list'),
+path('dashboard/products/add/', views.admin_product_create, name='admin_product_create'),
+path('dashboard/products/edit/<int:pk>/', views.admin_product_update, name='admin_product_update'),
+path('dashboard/products/delete/<int:pk>/', views.admin_product_delete, name='admin_product_delete'),
 ]
--- store/views.py
+++ store/views.py
@@ -12,6 +12,9 @@ from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.db.models import Sum
+from .models import Product
+from .forms import ProductForm
+from django.shortcuts import get_object_or_404
 
 def is_admin(user):
     return user.is_staff
@@ -22,6 +25,50 @@ def redirect_after_login(request):
         return redirect('admin_dashboard')
     else:
         return redirect('product_list')
+
+@user_passes_test(lambda u: u.is_staff)
+def admin_product_list(request):
+    products = Product.objects.all()
+    return render(request, 'admin/products/product_list.html', {'products': products})
+
+
+@user_passes_test(lambda u: u.is_staff)
+def admin_product_create(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('admin_product_list')
+    else:
+        form = ProductForm()
+
+    return render(request, 'admin/products/product_form.html', {'form': form})
+
+
+@user_passes_test(lambda u: u.is_staff)
+def admin_product_update(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            return redirect('admin_product_list')
+    else:
+        form = ProductForm(instance=product)
+
+    return render(request, 'admin/products/product_form.html', {'form': form})
+
+
+@user_passes_test(lambda u: u.is_staff)
+def admin_product_delete(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+
+    if request.method == 'POST':
+        product.delete()
+        return redirect('admin_product_list')
+
+    return render(request, 'admin/products/product_confirm_delete.html', {'product': product})
 
 @user_passes_test(lambda u: u.is_staff)
 def admin_dashboard(request):
